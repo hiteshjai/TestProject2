25faf7e31e5947f54eeca41c969ac36e03d2ad0d
hiteshjai <jain_hitesh85@yahoo.com>
2020-05-04 11:56:07 +0530

    Ms Test Project

---
+++ MstestProject/UnitTest1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace MstestProject
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            int i = 4;
+            int j = 2 + 2;
+            Assert.AreEqual(j, i);
+        }
+
+        [TestMethod]
+        public void TestStringCompare()
+        {
+            String input = "Test";
+            String output = "Test";
+            Assert.AreSame(input, output,"Strings are equal,Test passed");
+        }
+    }
+}
